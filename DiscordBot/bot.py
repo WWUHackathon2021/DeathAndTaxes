4c5bf144375bdf7e8808d8eb2eaed0def3a8f050
Daniel Wilson <danielwils@hotmail.com>
2021-04-10 15:35:06 -0700

    test command added

--- DiscordBot/bot.py
+++ DiscordBot/bot.py
@@ -13,4 +13,17 @@ client = discord.Client()
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
+
+#RESPOND TO MASSAGES HERE
+@client.event
+async def on_message(message):
+    #prevents bot from responding to self
+    if message.author == client.user:
+        return
+
+    #test
+    if message.content == '!test':
+        response = "testing testing 1 2 3"
+        await message.channel.send(response)
+
 client.run(TOKEN)
