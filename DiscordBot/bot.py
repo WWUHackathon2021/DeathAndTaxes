f3f9b27ad4a1d6f3c9070941cb089c85869f66b1
Shane Baden <naruto.shane@gmail.com>
2021-04-10 21:57:58 -0700

    pushing code

--- DiscordBot/bot.py
+++ DiscordBot/bot.py
@@ -7,6 +7,8 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 import importlib.util
+import random
+
 spec = importlib.util.spec_from_file_location("bees", "commands/bee_movie.py")
 bee_movie = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(bee_movie)
@@ -15,11 +17,19 @@ test = importlib.util.spec_from_file_location("ball", "commands/magic_8_ball.py"
 magic = importlib.util.module_from_spec(test)
 test.loader.exec_module(magic)
 
+doc = importlib.util.spec_from_file_location("accident", "commands/days_since_accident.py")
+days = importlib.util.module_from_spec(doc)
+doc.loader.exec_module(days)
+
 client = discord.Client()
 
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+
+thieyreChecker = ["There", "Their", "They're"] 
+thieyreList = ["Sorry dumbass, I think you meant ""their're"".", "Sorry shitter, I think you meant ""theyre   '"".", "Sorry dogwater sage main, I think you meant ""thiare"".", 
+"ACTUALLY?! It's PRONOUNCED ""thir""."]
 
 
 #RESPOND TO MASSAGES HERE
@@ -36,8 +46,19 @@ async def on_message(message):
 
     if message.content.partition(' ')[0] == '!8ball':
         await magic.ball(message)
+    
+    if message.content.partition(' ')[0] == '!track' or message.content.partition(' ')[0] == '!reset' or message.content.partition(' ')[0] == '!check':
+        await days.accident(message)
 
     if message.content == '!bees':
         await bee_movie.bees(message)
+
+    for content in thieyreChecker:
+        if content.lower() in message.content:
+            pick = random.randint(0,3)
+            response = thieyreList[pick]
+            await message.channel.send(response)
+
+    
 
 client.run(TOKEN)
