476452c768f87b9e0887670b009afbcc5ff404fe
Daniel Wilson <danielwils@hotmail.com>
2021-04-10 20:20:52 -0700

    8ball command updates

--- DiscordBot/commands/magic_8_ball.py
+++ DiscordBot/commands/magic_8_ball.py
@@ -1,4 +1,3 @@
-#magic_8_ball.py
 import random
 
 async def ball(message):
@@ -20,5 +19,7 @@ async def ball(message):
 
     ### General responses ###
     else:
-        responses = ['meh...', 'fuck if I know', 'no shit Sherlock', 'Is that a real question?', 'Survey Says: Fuck You', 'You know I think they teach that in like 3rd grade', 'In the words of Søren Aabye Kierkegaard, philosopher and father of existential humanism: Shut up and leave me alone', 'Absofuckinglutely', 'When hell freezes over', 'Idk I can\'t check right now, my wifi is out', 'Oh hell yeah', 'I mean I think so but I heard it from this guy who won\'t shut up about the moon landings so take that with a grain of salt', 'One sec I\'m taking a hit', 'Sure, one sec.\nHey Siri!\nSome dumbass wants to know ' + message.content.split(' ', 1)[1] + '\nApparently the answer is no']
-        await message.channel.send(responses[random.randint(0,13)])
+        responses = ['meh...', 'fuck if I know', 'no shit Sherlock', 'Is that a real question?', 'Survey Says: Fuck You', 'You know I think they teach that in like 3rd grade', 'In the words of Søren Aabye Kierkegaard, philosopher and father of existential humanism: Shut up and leave me alone', 'Absofuckinglutely', 'When hell freezes over', 'Idk I can\'t check right now, my wifi is out', 'Oh hell yeah', 'I mean I think so but I heard it from this guy who won\'t shut up about the moon landings so take that with a grain of salt', 'One sec I\'m taking a hit', 'Sure, one sec.\nHey Siri!\nSome dumbass wants to know ' + (message.content+' ').split(' ', 1)[1] + '\nApparently the answer is no', 'You\’re gonna want to take a seat for this one: No.', 'Sure… maybe. Maybe one day we’ll have peace on earth. Maybe Mitch McConnel will sing lead in a 70-plus NWA cover band. And yeah, maybe one day, your thing. What can I say? I’m an optimist.', 'No. And I don\’t just mean \"Sir, have you been drinking tonight?\" no. I mean, like, \"Do you think she still thinks about me?\" no.', 'Sometimes I hit the nail right on the head. Other times I\’m just hammered. Ask tomorrow.', 'Remember the words of the twenty-first century poet, Usher: \“Yeah.\”', 'Fuck yeah, bro. Don\’t even trip.', 'I gotchu.']
+
+
+        await message.channel.send(responses[random.randint(0,20)])
