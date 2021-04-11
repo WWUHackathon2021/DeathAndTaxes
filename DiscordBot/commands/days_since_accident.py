0d011960b429a2fa336f4991fd1dd8ca6baf1f6b
Shane Baden <naruto.shane@gmail.com>
2021-04-11 05:04:20 -0700

    New days since accident command

--- DiscordBot/commands/days_since_accident.py
+++ DiscordBot/commands/days_since_accident.py
@@ -1,13 +1,37 @@
 #days_since_accident.py
 import random
+import datetime
+
+trackedMessages = []
+trackedTimes = []
 
 async def accident(message):
 
     ### Check for content ###
+    now = datetime.datetime.now()
 
+    content = message.content.replace("!track ", "")
+    content = content.replace("!reset ", "")
+    content = content.replace("!check ", "")
+    
     if message.content.partition(' ')[0] == '!track':
-        message.channel.send("not implemented")
+        trackedMessages.append(content)
+        trackedTimes.append(now)
+        await message.channel.send("Tracking time since " + content + ". Feh. good luck with that shit, we know how this goes. *rolls eyes*.")
+
+    pos = trackedMessages.index(content)
+    time_delta = (now - trackedTimes[pos])
+    change_seconds = time_delta.total_seconds()/1
+    change_minutes = change_seconds/60
+    change_hours = change_minutes/60
+    change_days = change_hours/24
+    time_string = str(change_days) + " days, " + str(change_hours) + " hours, " + str(change_minutes) + " minutes, " + str(change_seconds) + " seconds."
+
     if message.content.partition(' ')[0] == '!reset':
-        message.channel.send("not implemented")
+        pos = trackedMessages.index(content)
+        await message.channel.send("Wow, its only been " + time_string + " since " + content + ". Way to 'break' the cycle =_=. Everyone was REALLLLY expecting this to succeed *spits in your direction*.")
+        trackedTimes[pos] = now
+
     if message.content.partition(' ')[0] == '!check':
-        message.channel.send("not implemented")
+        pos = trackedMessages.index(content)
+        await message.channel.send("It has been " + time_string + " since " + content + ". good luck warrior, Something about this is telling me you can break this habit :).")
